19fcb8e4eabbdae372223f7382c1bb565acbe831
Suvendu <suvendu.banerjee80@gmail.com>
2017-01-11 02:02:51 +0530

    REFACTOR : remove more local variable

--- tennis-kata/src/main/java/TennisGame1.java
+++ tennis-kata/src/main/java/TennisGame1.java
@@ -1,6 +1,10 @@
 
 public class TennisGame1 implements TennisGame {
 
+	private static final String WIN_FOR = "Win for ";
+	private static final String ADVANTAGE = "Advantage ";
+	private static final String PLAYER1 = "player1";
+	private static final String PLAYER2 = "player2";
 	private static final String ALL_SUFFIX = "-All";
 	private int player1Score = 0;
 	private int player2Score = 0;
@@ -9,7 +13,7 @@ public class TennisGame1 implements TennisGame {
 	}
 
 	public void wonPoint(String playerName) {
-		if (playerName == "player1")
+		if (playerName == PLAYER1)
 			player1Score += 1;
 		else
 			player2Score += 1;
@@ -20,24 +24,20 @@ public class TennisGame1 implements TennisGame {
 			return tie();
 		} else if (player1Score >= 4 || player2Score >= 4) {
 			return scoreAboveFour();
-		} 
-			return scoreToNameMap(this.player1Score) + "-" + scoreToNameMap(this.player2Score);
+		}
+		return Point.values()[this.player1Score] + "-" + Point.values()[this.player2Score];
 	}
 
-	private String scoreToNameMap(int tempScore) {
-		return Point.values()[tempScore].toString();
+	private String advantage(int scoreDiffrence) {
+		return scoreDiffrence == 1 ? ADVANTAGE + PLAYER1 : ADVANTAGE + PLAYER2;
 	}
 
 	private String scoreAboveFour() {
 		int scoreDiffrence = player1Score - player2Score;
-		if (scoreDiffrence == 1)
-			return "Advantage player1";
-		else if (scoreDiffrence == -1)
-			return "Advantage player2";
-		else if (scoreDiffrence >= 2)
-			return "Win for player1";
-		else
-			return"Win for player2";
+		if (Math.abs(scoreDiffrence) == 1) {
+			return advantage(scoreDiffrence);
+		}
+		return scoreDiffrence >= 2 ? WIN_FOR + PLAYER1 : WIN_FOR + PLAYER2;
 	}
 
 	private String tie() {
