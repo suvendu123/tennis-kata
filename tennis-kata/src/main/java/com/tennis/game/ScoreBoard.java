04bb6d546665b9a23e08e74281ded161838e5774
Suvendu <suvendu.banerjee80@gmail.com>
2017-01-11 03:28:52 +0530

    final refactor of TennisGame1

---
+++ tennis-kata/src/main/java/com/tennis/game/ScoreBoard.java
@@ -0,0 +1,67 @@
+package com.tennis.game;
+
+import static com.tennis.game.Point.*;
+import static java.lang.Math.abs;
+
+/**
+ * @author sbandy
+ *
+ */
+public class ScoreBoard {
+
+	private static final String WIN_FOR = "Win for ";
+	private static final String ADVANTAGE = "Advantage ";
+	private static final String ALL_SUFFIX = "-All";
+	private Player player1;
+	private Player player2;
+
+	public ScoreBoard(Player player1, Player player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+
+	public Player getPlayer1() {
+		return player1;
+	}
+
+	public void setPlayer1(Player player1) {
+		this.player1 = player1;
+	}
+
+	public Player getPlayer2() {
+		return player2;
+	}
+
+	public void setPlayer2(Player player2) {
+		this.player2 = player2;
+	}
+
+	public boolean isTie() {
+		return player1.getScore() == player2.getScore();
+	}
+
+	public String displayTie() {
+		return player1.getScore() < 3 ? values()[player1.getScore()].toString() + ALL_SUFFIX : Deuce.toString();
+	}
+
+	public boolean isAboveFour() {
+		return player1.getScore() >= 4 || player2.getScore() >= 4;
+	}
+
+	public String displayAboveFour() {
+		int scoreDiffrence = player1.getScore() - player2.getScore();
+		if (abs(scoreDiffrence) == 1) {
+			return advantage(scoreDiffrence);
+		}
+		return scoreDiffrence >= 2 ? WIN_FOR + player1.getName() : WIN_FOR + player2.getName();
+	}
+
+	private String advantage(int scoreDiffrence) {
+		return scoreDiffrence == 1 ? ADVANTAGE + player1.getName() : ADVANTAGE + player2.getName();
+	}
+
+	public String displayScore() {
+		return values()[player1.getScore()] + "-" + values()[player2.getScore()];
+	}
+
+}
--- tennis-kata/src/main/java/com/tennis/game/TennisGame1.java
+++ tennis-kata/src/main/java/com/tennis/game/TennisGame1.java
@@ -1,57 +1,29 @@
 package com.tennis.game;
-import static com.tennis.game.Point.*;
-import static java.lang.Math.abs;
+
 public class TennisGame1 implements TennisGame {
 
-	private static final int PLAYER_2 = 1;
-	private static final int PLAYER_1 = 0;
-	private static final String WIN_FOR = "Win for ";
-	private static final String ADVANTAGE = "Advantage ";
-	private static final String ALL_SUFFIX = "-All";
-
-	private Player[] players = new Player[2];
+	private ScoreBoard scoreBoard;
 
 	public TennisGame1(String player1Name, String player2Name) {
-		players[PLAYER_1] = new Player(player1Name, 0);
-		players[PLAYER_2] = new Player(player2Name, 0);
+
+		this.scoreBoard = new ScoreBoard(new Player(player1Name, 0), new Player(player2Name, 0));
 	}
 
 	public void wonPoint(String playerName) {
-		if (players[PLAYER_1].getName().equals(playerName)) {
-			players[PLAYER_1].addScore();
+		if (scoreBoard.getPlayer1().getName().equals(playerName)) {
+			scoreBoard.getPlayer1().addScore();
 		} else {
-			players[PLAYER_2].addScore();
+			scoreBoard.getPlayer2().addScore();
 		}
 	}
 
 	public String getScore() {
-		return calculateScore(players[PLAYER_1], players[PLAYER_2]);
-	}
-
-	private String calculateScore(Player player1, Player player2) {
-		if (player1.getScore() == player2.getScore()) {
-			return tie(player1);
-		} else if (player1.getScore() >= 4 || player2.getScore() >= 4) {
-			return scoreAboveFour(player1, player2);
+		if (scoreBoard.isTie()) {
+			return scoreBoard.displayTie();
+		} else if (scoreBoard.isAboveFour()) {
+			return scoreBoard.displayAboveFour();
 		}
-		return values()[player1.getScore()] + "-" + values()[player2.getScore()];
+		return scoreBoard.displayScore();
 	}
 
-	private String advantage(int scoreDiffrence, Player player1, Player player2) {
-		return scoreDiffrence == 1 ? ADVANTAGE + player1.getName() : ADVANTAGE + player2.getName();
-	}
-
-	private String scoreAboveFour(Player player1, Player player2) {
-		int scoreDiffrence = player1.getScore() - player2.getScore();
-		if (abs(scoreDiffrence) == 1) {
-			return advantage(scoreDiffrence, player1, player2);
-		}
-		return scoreDiffrence >= 2 ? WIN_FOR + player1.getName() : WIN_FOR + player2.getName();
-	}
-
-	private String tie(Player player1) {
-		return player1.getScore() < 3 ? Point.values()[player1.getScore()].toString() + ALL_SUFFIX
-				: Deuce.toString();
-
-	}
 }
